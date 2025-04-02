4f45bc411647a93fc35eb1fdf4b87fdb6b6d0b46
shiboyu1999 <shiboyu.seu@gmail.com>
2025-04-02 16:16:11 +0800

    debug

--- scripts/LG_Extract_Vit.sh
+++ scripts/LG_Extract_Vit.sh
@@ -7,12 +7,12 @@ BATCH_SIZE=32
 NUM_EPOCHS_PER_TASK=30
 LR=0.0001
 NUM_WORKERS=4
-SAVE_PATH="/home/shiboyu/code/02-NCP/results/Grad-vit-checkpoints/"
+SAVE_PATH="/home/shiboyu/code/02-NCP-1/results/Grad-vit-checkpoints/"
 DATASET_ROOT="/home/shiboyu/dataset/ILSVRC2012/train"
 ARCH="vit-base"
 
 # 启动训练
-python /home/shiboyu/code/02-NCP/LG-Extract/CNN-Grad.py \
+python /home/shiboyu/code/02-NCP-1/LG-Extract/ViT-Grad.py \
     --num-tasks $NUM_TASKS \
     --class-per-task $CLASS_PER_TASK \
     --batch-size $BATCH_SIZE \
