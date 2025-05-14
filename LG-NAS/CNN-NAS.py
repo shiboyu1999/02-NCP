ded10c4bcd319c8057696bc6c006afd7c6c0cdaa
shiboyu1999 <shiboyu.seu@gmail.com>
2025-05-14 13:09:56 +0800

    renew

--- LG-NAS/CNN-NAS.py
+++ LG-NAS/CNN-NAS.py
@@ -1,23 +1,31 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as f
+import torch.nn.functional as F
+import torchvision.models as models
+import os
+from datetime import datetime
+import argparse
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
 
-class BasicBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, stride=1):
-        super(BasicBlock, self).__init__()
-        padding = kernel_size // 2  # out_size = (in_size + padding * 2 - kernel_size) / stride + 1
+class Bottleneck(nn.Module):
+    expansion = 4
 
-        # 第一个卷积层
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size, stride=stride, padding=padding, bias=False)
-        self.bn1 = nn.BatchNorm2d(out_channels)
+    def __init__(self, in_channels, out_channels, stride=1):
+        super(Bottleneck, self).__init__()
+        mid_channels = out_channels // self.expansion
 
-        # 第二个卷积层
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size, stride=1, padding=padding, bias=False)
-        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.conv1 = nn.Conv2d(in_channels, mid_channels, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(mid_channels)
 
-        # 残差连接
+        self.conv2 = nn.Conv2d(mid_channels, mid_channels, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(mid_channels)
+
+        self.conv3 = nn.Conv2d(mid_channels, out_channels, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(out_channels)
+
         self.shortcut = nn.Sequential()
-        if stride !=1  or in_channels != out_channels:
+        if stride != 1 or in_channels != out_channels:
             self.shortcut = nn.Sequential(
                 nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
                 nn.BatchNorm2d(out_channels)
@@ -25,75 +33,217 @@ class BasicBlock(nn.Module):
 
     def forward(self, x):
         identity = x
-        y = f.relu(self.bn1(self.conv1(x)))
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = F.relu(self.bn2(self.conv2(out)))
+        out = self.bn3(self.conv3(out))
+        out += self.shortcut(identity)
+        return F.relu(out)
+
+
+class CandBottleneckBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1, num_bottleneck=1):
+        super(CandBottleneckBlock, self).__init__()
+        layers = []
+        for i in range(num_bottleneck):
+            layers.append(
+                Bottleneck(
+                    in_channels if i == 0 else out_channels,
+                    out_channels,
+                    stride if i == 0 else 1
+                )
+            )
+        self.block = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.block(x)
+
+
+# ----------- BasicBlock ----------
+class BasicBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1):
+        super(BasicBlock, self).__init__()
+        padding = kernel_size // 2
+
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size, stride=stride, padding=padding, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size, stride=1, padding=padding, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_channels != out_channels:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_channels)
+            )
+
+    def forward(self, x):
+        identity = x
+        y = F.relu(self.bn1(self.conv1(x)))
         y = self.bn2(self.conv2(y))
-
         out = y + self.shortcut(identity)
-        out = f.relu(out)
-        return out
+        return F.relu(out)
 
+# ---------- CandBasicBlock ----------
 class CandBasicBlock(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride=1, num_conv=1):
         super(CandBasicBlock, self).__init__()
         self.num_conv = num_conv
-        padding = kernel_size // 2  # out_size = (in_size + padding * 2 - kernel_size) / stride + 1 
+        self.candidate_block = nn.Sequential()
 
-        self.candidate_block = nn.ModuleList()
+        layers = []
+        for i in range(num_conv):
+            layers.append(
+                BasicBlock(in_channels if i == 0 else out_channels, out_channels, kernel_size, stride if i == 0 else 1)
+            )
+        self.candidate_block = nn.Sequential(*layers)
 
-        # 单层卷积
-        if num_conv == 1:
-            self.block1 = BasicBlock(in_channels, out_channels, kernel_size, stride=stride)
-            self.candidate_block.append(self.block1)
-        
-        elif num_conv == 2:
-            self.block2_1 = BasicBlock(in_channels, out_channels, kernel_size, stride=stride)
-            self.block2_2 = BasicBlock(out_channels, out_channels, kernel_size, stride=stride)
-            self.candidate_block.append(self.block2_1)
-            self.candidate_block.append(self.block2_2)
-        
-        elif num_conv == 3:
-            self.block3_1 = BasicBlock(in_channels, out_channels, kernel_size, stride=stride)
-            self.block3_2 = BasicBlock(out_channels, out_channels, kernel_size, stride=stride)
-            self.block3_3 = BasicBlock(out_channels, out_channels, kernel_size, stride=stride)
-            self.candidate_block.append(self.block3_1)
-            self.candidate_block.append(self.block3_2)
-            self.candidate_block.append(self.block3_3)
-        
-        else:
-            raise ValueError("num_conv must be 1, 2 or 3!")
-        
     def forward(self, x):
-        for layer in self.candidate_block:
-            x = layer(x)
-        return x
+        return self.candidate_block(x)
 
+# ---------- SuperNet ----------
 class SuperNet(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, stride, num_layers=4):
+    def __init__(self, block_type="basic"):
         super(SuperNet, self).__init__()
-        # 初始卷积层
-        self.conv1 = nn.Conv2d(in_channels, out_channels=64, kernel_size=7, stride=2, padding=1, bias=False)
+
+        BlockClass = BasicBlock if block_type == "basic" else Bottleneck
+        CandBlockClass = CandBasicBlock if block_type == "basic" else CandBottleneckBlock
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
         self.bn1 = nn.BatchNorm2d(64)
-        self.maxpool = nn.Maxpool(kernel_size=3, stride=2, padding=1)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
 
-        # 候选层
-        for i in range(num_layers):
-            setattr(self, f'candlayer_{i}', nn.ModuleList())
-            current_candlayer = getattr(self, f'candlayer_{i}')
-            current_candlayer.append(CandBasicBlock(in_channels, out_channels, kernel_size=j, num_conv=1) for j in [1, 3, 5, 7])
-            current_candlayer.append(CandBasicBlock(in_channels, out_channels, kernel_size=j, num_conv=2) for j in [1, 3, 5, 7])
-            current_candlayer.append(CandBasicBlock(in_channels, out_channels, kernel_size=j, num_conv=3) for j in [1, 3, 5, 7])
-    
-    def forward(self, x_0, x_1, x_2, x_3):
-        out_1 = [self.candlayer_0[i](x_0) for i in range(12)]
-        out_2 = [self.candlayer_1[i](x_1) for i in range(12)]
-        out_3 = [self.candlayer_2[i](x_2) for i in range(12)]
-        out_4 = [self.candlayer_3[i](x_3) for i in range(12)]
-        return out_1, out_2, out_3, out_4
+        self.candlayer_0 = nn.ModuleList([CandBlockClass(64, 64, num_conv=n) for n in [1, 2, 3]])
+        self.candlayer_1 = nn.ModuleList([CandBlockClass(64, 128, num_conv=n) for n in [1, 2, 3]])
+        self.candlayer_2 = nn.ModuleList([CandBlockClass(128, 256, num_conv=n) for n in [1, 2, 3]])
+        self.candlayer_3 = nn.ModuleList([CandBlockClass(256, 512, num_conv=n) for n in [1, 2]])
 
-def train_supernet(supernet, ansnet, train_loader):
+    def forward(self, img, *t_feats):
+        assert len(t_feats) >= 4
+
+        x0 = F.relu(self.bn1(self.conv1(img)))
+        x0 = self.maxpool(x0)
+        out0 = [self.candlayer_0[i](x0) for i in range(len(self.candlayer_0))]
+        out1 = [self.candlayer_1[i](t_feats[0]) for i in range(len(self.candlayer_1))]
+        out2 = [self.candlayer_2[i](t_feats[1]) for i in range(len(self.candlayer_2))]
+        out3 = [self.candlayer_3[i](t_feats[2]) for i in range(len(self.candlayer_3))]
+        return out0, out1, out2, out3
+
+
+def train_supernet(supernet, teacher_model, train_loader, device, log_path="supernet_log.txt", epochs=10):
+    supernet.to(device)
+    teacher_model.to(device)
+    teacher_model.eval()
+
     optimizer = torch.optim.Adam(supernet.parameters(), lr=0.001)
-    criterion = nn.mseloss()
+    criterion = nn.MSELoss()
+
+    # 日志文件初始化
+    os.makedirs(os.path.dirname(log_path), exist_ok=True)
+    with open(log_path, "w") as f:
+        f.write(f"SuperNet Training Log - {datetime.now()}\n\n")
+
+    for epoch in range(epochs):
+        total_loss = 0.0
+        for step, (images, _) in enumerate(train_loader):
+            images = images.to(device)
+
+            with torch.no_grad():
+                t_out = teacher_model(images)
+                t_feats = [t_out[i] for i in [0, 1, 2, 3]]  # 假设你能访问中间输出
+
+            s_out = supernet(images, *t_feats)
+
+            loss = 0.0
+            for s_layer, t_layer in zip(s_out, t_feats):
+                for s_block in s_layer:
+                    loss += criterion(s_block, t_layer)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            total_loss += loss.item()
+            if step % 10 == 0:
+                log = f"[Epoch {epoch+1}/{epochs}] Step {step}, Loss: {loss.item():.4f}"
+                print(log)
+                with open(log_path, "a") as f:
+                    f.write(log + "\n")
+
+        avg_loss = total_loss / len(train_loader)
+        epoch_log = f"==> Epoch [{epoch+1}/{epochs}] Average Loss: {avg_loss:.4f}\n"
+        print(epoch_log)
+        with open(log_path, "a") as f:
+            f.write(epoch_log)
+
+def get_args():
+    parser = argparse.ArgumentParser(description="Train SuperNet with teacher model")
+
+    parser.add_argument('--batch_size', type=int, default=64, help='Batch size for training')
+    parser.add_argument('--learning_rate', type=float, default=0.001, help='Initial learning rate')
+    parser.add_argument('--epochs', type=int, default=10, help='Number of epochs')
+    parser.add_argument('--log_path', type=str, default='logs/supernet_log.txt', help='Log file path')
+    parser.add_argument('--device', type=str, default='cuda' if torch.cuda.is_available() else 'cpu', help='Device to use for training')
+    parser.add_argument('--dataset_root', type=str, default='./data', help='Path to download/load dataset')
+    parser.add_argument('--arch', type=str, default='resnet34', choices=['resnet34', 'resnet50'], help='Teacher model architecture')
 
 
+    return parser.parse_args()
+
+def main():
+    args = get_args()
+
+    # 数据预处理
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5,), (0.5,))
+    ])
+
+    # 加载 ImageFolder 数据集
+    train_dataset = ImageFolder(root=args.dataset_root, transform=transform)
+    train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=4)
+
+    # 根据 arch 决定教师模型和 supernet 结构
+    if args.arch == 'resnet34':
+        teacher = models.resnet34(pretrained=True)
+        block_type = "basic"
+    elif args.arch == 'resnet50':
+        teacher = models.resnet50(pretrained=True)
+        block_type = "bottleneck"
+    else:
+        raise ValueError(f"Unsupported architecture: {args.arch}")
+
+    teacher_model = ResNetWithFeatures(teacher)
+    supernet = SuperNet(block_type=block_type)
+
+    # 开始训练
+    train_supernet(
+        supernet, 
+        teacher_model, 
+        train_loader, 
+        device=args.device, 
+        log_path=args.log_path, 
+        epochs=args.epochs
+    )
+
+class ResNetWithFeatures(nn.Module):
+    def __init__(self, backbone):
+        super().__init__()
+        self.layer0 = nn.Sequential(backbone.conv1, backbone.bn1, backbone.relu, backbone.maxpool)
+        self.layer1 = backbone.layer1
+        self.layer2 = backbone.layer2
+        self.layer3 = backbone.layer3
+        self.layer4 = backbone.layer4
+
+    def forward(self, x):
+        x = self.layer0(x)
+        f1 = self.layer1(x)
+        f2 = self.layer2(f1)
+        f3 = self.layer3(f2)
+        f4 = self.layer4(f3)
+        return [f1, f2, f3, f4]
 
 
+if __name__ == '__main__':
+    main()
---
+++ scripts/CNN_NAS.sh
@@ -0,0 +1,6 @@
+python /home/biye/code/02-NCP-1/LG-NAS/CNN-NAS.py.py \
+        --batch_size 128 \
+        --learning_rate 0.0005 \
+        --epochs 20 \
+        --log_path ./results/CNN-NAS/resnet-34/logs/mylog.txt \
+        --arch resnet34
