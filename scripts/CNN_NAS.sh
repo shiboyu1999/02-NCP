0b60660dceeeb4fb95294beab4abfbda904df91d
shiboyu1999 <shiboyu.seu@gmail.com>
2025-05-15 12:35:03 +0800

    chore: Update batch size to 32 in CNN-NAS.sh

--- scripts/CNN_NAS.sh
+++ scripts/CNN_NAS.sh
@@ -1,5 +1,5 @@
 CUDA_VISIBLE_DEVICES=1 python /home/biye/code/02-NCP-1/LG-NAS/CNN-NAS.py \
-        --batch_size 20 \
+        --batch_size 32 \
         --learning_rate 0.0005 \
         --epochs 20 \
         --log_path ./results/CNN-NAS/resnet-34/logs/ \
